ca447c647b9b1298ec2050d2f5359fa21c7be993
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2016-11-15 09:30:37 +0100

    removed keword args from a2pr.py

--- a2pr.py
+++ a2pr.py
@@ -1,35 +1,28 @@
 
-def a2pr(A, *args, **kwargs):
-    nargin = sys._getframe(1).f_locals['nargin']
-    varargin = sys._getframe(1).f_locals['varargin']
-    nargout = sys._getframe(1).f_locals['nargout']
+def a2pr(A):
+    '''Pitch and roll estimator for DTAG data.
 
-    #     [p,r,v] = a2pr(A)
-#     Pitch and roll estimator for DTAG data. This is a simple
-#     non-iterative estimator with |pitch| constrained to <= 90 degrees.
-#     The p & r estimates give the least-square-norm error between A and
-#     the A-vector that would be measured at the estimated pitch and roll.
+    This is a simple non-iterative estimator with |pitch| constrained to <= 90
+    degrees.  The p & r estimates give the least-square-norm error between A
+    and the A-vector that would be measured at the estimated pitch and roll.
 
-    #     Inputs:
-#     A is a nx3 acceleration matrix
+    Inputs:
+    A is a nx3 acceleration matrix
 
-    #     Outputs:
-#     p is the pitch estimate in radians
-#     r is the roll estimate in radians
-#     v is the 2-norm of the acceleration measurements
+    Outputs:
+    p is the pitch estimate in radians
+    r is the roll estimate in radians
+    v is the 2-norm of the acceleration measurements
+    '''
+    import numpy
 
-    #     mark johnson, WHOI
-#     majohnson@whoi.edu
-#     last modified: 24 Nov. 2005
+    if min(cat(A.shape[0], A.shape[1])) == 1:
+        A = A.ravel().T
 
-    if nargin == 0:
-        help('a2p')
-        return p, r, v
+    v = numpy.sqrt(A**2 *  [1,1,1])
 
-    if min(cat(size(A, 1), size(A, 2))) == 1:
-        A = ravel(A).T
-
-    v = sqrt(dot(A ** 2, cat([1], [1], [1])))
     # compute pitch and roll
-    p = asin(A[:, 1] / v)
-    r = real(atan2(A[:, 2], A[:, 3]))
+    p = numpy.arcsin(A[:, 0] / v)
+    r = float(numpy.arctan2(A[:, 1], A[:, 2]))
+
+    return p, r, v
