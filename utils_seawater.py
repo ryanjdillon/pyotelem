3cd5ad42741a2cd74bf492a1e9aaf9828ff3c0cb
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2016-11-18 12:29:57 +0100

    moved EstimateDsw routine to utils_seawater module

--- utils_seawater.py
+++ utils_seawater.py
@@ -1,3 +1,18 @@
+def EstimateDsw(SWdensity, depCTD, p):
+    '''Estimate seawater density from CTD measurement
+
+    Args
+    ----
+    SWdensity: seawater density from CTD measurement
+    depCTD: CTD's depth data where SWdensity was recorded
+    p: animal's depth data
+
+    Returns
+    -------
+    Dsw: density of seawater
+    '''
+    return interp1(depCDT, SWdensity, p)
+
 
 def SWdensityFromCTD(DPT, TMP, SL, D):
     '''Calculate seawater density at CTD depth'''
