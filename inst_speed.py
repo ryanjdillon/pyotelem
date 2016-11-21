9f2b4ab497e1fd5299f9561a922451f8948899e2
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2016-11-21 16:00:17 +0100

    updates to python formatting for inst_speed.py

--- inst_speed.py
+++ inst_speed.py
@@ -1,44 +1,73 @@
 
-def inst_speed(p, smoothpitch, fs, FR, f, k, thdeg, *args, **kwargs):
-    nargin = sys._getframe(1).f_locals['nargin']
-    varargin = sys._getframe(1).f_locals['varargin']
-    nargout = sys._getframe(1).f_locals['nargout']
+def inst_speed(depths, smoothpitch, fs, stroke_f, f, ind, thresh_deg):
+    '''Estimate instantaneous swimming speed as depthrate
 
-    #    [SwimSp] = inst_speed(p,smoothpitch,fs,FR,f,k,thdeg)
-#    Estimate instantaneous swimming speed as depthrate
-#
-#    INPUT:
-#       p = the depth time series in meters, sampled at fs Hz.
-#       smoothpitch = pitch estimated from the low pass filtered
-#       acceleration signal
-#       fs = sensor sampling rate in Hz
-#       FR = equal to the nominal stroking rate in Hz. Default value is 0.5
-#           Hz. Use [] to get the default value if you want to enter
-#           subsequent arguments.
-#       f = number that multiplied by the FR gives the cut-off frequency fl,
-#           of the low pass filter. f is a fraction of FR e.g., 0.4.
-#       k = sample range over which to analyse.
-#       thdeg = degrees threshold for which the speed will be estimated
+    Args
+    ----
+    depths:
+        the depth time series in meters, sampled at fs Hz.
+    smoothpitch:
+        pitch estimated from the low pass filtered acceleration signal
+    fs:
+        sensor sampling rate in Hz
+    stroke_f:
+        equal to the nominal stroking rate in Hz. Default value is 0.5
+        Hz. Use [] to get the default value if you want to enter
+        subsequent arguments.
+    f:
+        number that multiplied by the stroke_f gives the cut-off frequency Wn,
+        of the low pass filter. f is a fraction of stroke_f e.g., 0.4.
+    ind:
+        sample range over which to analyse.
+    thresh_deg:
+        degrees threshold for which the speed will be estimated
 
-    #    OUTPUT:
-#          Instspeed = instantaneous speed calculated as the depthrate.
+    Returns
+    -------
+    inst_speed:
+        instantaneous speed calculated as the depth rate.
 
-    #   Lucia Martina Martin Lopez (May 2016)
-#   lmml2@st-andrews.ac.uk
+    Notes
+    -----
+    `p`  changed to `depths`
+    `fl` changed to `Wn`
+    `k`  changed to `ind`
+    `Instspeed` changed to `inst_speed`
+    `SwimSp` changed to `swim_speed`
 
-    # define the cut off frequency (fc) for the low-pass filter
-    fc = (dot(f, FR))
-    # fl is the filter cut-off normalized to half the sampling frequency
-# (Nyquist frequency).
-    fl = fc / (fs / 2)
+    Lucia Martina Martin Lopez (May 2016)
+    lmml2@st-andrews.ac.uk
+    '''
+    import numpy
+
+    import utils_signal
+
+    # TODO default stroke_f rate 0.5 here, 0.4 elsewhere
+
+    # define the cut off frequency (cutoff) for the low-pass filter
+    cutoff = f * stroke_f
+
+    # Wn is the filter cut-off normalized to half the sampling frequency
+    # (a.k.a the critical frequency).
+    nyq = 0.5 * fs
+    Wn  = cutoff / nyq
+
     # define the length of symmetric FIR (Finite Impulse Response) filter.
-    nf = round(dot(fs / fc, 4))
-    # apply a symmetric FIR low-pass filter to Aw and Mw with 0 group delay to
-# obtain the low-pass filtered acceleration signal Alf
+    n_f = round((fs / cutoff) * 4)
 
-    newspeed = dot(diff(p[k]), fs)
-    Instspeed = fir_nodelay(newspeed, nf, fl)
-    InstSpeed = matlabarray(cat([Instspeed], [NaN]))
-    SwimSp = - InstSpeed / smoothpitch
-    xa = copy(smoothpitch)
-    SwimSp[abs(dot(xa, 180) / pi) < thdeg] = NaN
+    # apply a symmetric FIR low-pass filter to Aw and Mw with 0 group delay to
+    # obtain the low-pass filtered acceleration signal Alf
+    new_speed = numpy.diff(depths[ind]) * fs
+
+    inst_speed = utils_signal.fir_nodelay(new_speed, n_f, Wn)
+    InstSpeed  = numpy.hstack((inst_speed, numpy.nan))
+
+    swim_speed = -InstSpeed / smoothpitch
+    xa         = numpy.copy(smoothpitch)
+    swim_speed[abs(numpy.rad2deg(xa)) < thresh_deg] = numpy.nan
+
+    # TODO
+    # Docstring states inst_speed & swim_speed returned
+    # [swim_speed] = inst_speed(depths, smoothpitch, fs, stroke_f, f, ind, thresh_deg)
+
+    return swim_speed
