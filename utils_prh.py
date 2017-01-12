f9b9f427282fe49f95a783725c8af2bc40288785
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-01-12 13:56:47 +0100

    var name changes and new improved speed estimation routine"

--- utils_prh.py
+++ utils_prh.py
@@ -1,11 +1,11 @@
 
-def calc_PRH(A_g):
+def calc_PRH(ax, ay, az):
     '''Calculate the pitch, roll and heading for triaxial movement signalsi
 
     Args
     ----
     A_g: numpy.ndarray, shape (n,3)
-        triaxial movement array with n samples over 3 axes (0:x, 1:y, 2:z)
+        triaxial movement array with n samples over 3 axes (0:ax, 1:ay, 2:az)
 
     Returns
     -------
@@ -17,34 +17,35 @@ def calc_PRH(A_g):
         heading in radians
     '''
 
-    p = pitch(A_g[:,0], A_g[:,1], A_g[:,2])
-    r  = roll(A_g[:,0], A_g[:,1], A_g[:,2])
-    y   = yaw(A_g[:,0], A_g[:,1], A_g[:,2])
+    pitch = calc_pitch(ax, ay, az)
+    roll  = calc_roll(ax, ay, az)
+    yaw   = calc_yaw(ax, ay, az)
 
     #from dtag_toolbox_python.dtag2 import a2pr
     #pitch, roll, A_norm = a2pr.a2pr(A_g)
 
-    return p, r, y
+    return pitch, roll, yaw
 
 
-def pitch(x,y,z):
+def calc_pitch(ax, ay, az):
     '''Angle of x-axis relative to ground (theta)'''
     import numpy
-    return numpy.arctan(x/numpy.sqrt(y**2+z**2))
+    # arctan2 not needed here to cover all quadrants, just for consistency
+    return numpy.arctan(ax, numpy.sqrt(ay**2+az**2))
 
 
-def roll(x,y,z):
+def calc_roll(ax, ay, az):
     '''Angle of y-axis relative to ground (phi)'''
     import numpy
-    #return numpy.arctan(y/numpy.sqrt(x**2+z**2))
-    return numpy.arctan2(y,z)
+    #return numpy.arctan(ay/numpy.sqrt(ax**2+az**2))
+    return numpy.arctan2(ay,az)
 
 
-def yaw(x,y,z):
+def calc_yaw(ax, ay, az):
     '''Angle of z-axis relative to ground (psi)'''
     import numpy
-    #return numpy.arctan(numpy.sqrt(x**2+y**2)/z)
-    return numpy.arctan2(x,y)
+    #return numpy.arctan(numpy.sqrt(ax**2+ay**2)/az)
+    return numpy.arctan2(ax,ay)
 
 
 def absdeg(deg):
@@ -63,3 +64,102 @@ def absdeg(deg):
         d[deg < 0] = 180 + deg[deg < 0]
 
     return d
+
+
+def acceleration_magnitude(ax, ay, az):
+    '''Cacluate the magnitude of 3D acceleration
+
+    http://physics.stackexchange.com/a/41655/126878
+    '''
+    import numpy
+    return numpy.sqrt(ax**2 + ay**2 + az**2)
+
+
+def triaxial_integration(x, y, z, initial=0):
+    '''Integrate three axes to obtain velocity of position'''
+    import scipy.integrate
+
+    x_int = scipy.integrate.cumtrapz(range(len(x)), x, initial=initial)
+    y_int = scipy.integrate.cumtrapz(range(len(y)), y, initial=initial)
+    z_int = scipy.integrate.cumtrapz(range(len(z)), z, initial=initial)
+
+    return x_int, y_int, z_int
+
+
+def speed_from_acc_and_ref(x, fs_a, rel_speed, zero_level, theoretic_max,
+        rm_neg=True):
+    '''Estimate speed from x-axis acceleration, fitting to relative speed data
+
+    Args
+    ----
+    x: ndarray
+        x-axis acceleromter data, same axis of movement as relative speed sensor
+    fs_a: float
+        sampling frequency of accelerometer sensor
+    rel_speed: ndarray
+        relative speed sensor data at same sampling freqency as accelerometer
+    zero_level: float
+        threshold at which relative speed data should be considered zero m/s^2
+    theoretic_max: float
+        theoretical maximum speed of the animal
+
+    Returns
+    -------
+    x_speed_cal: ndarray
+        relative speed data calibrated to the curve fit to the integrated speed
+        from the accelerometer data
+
+    Note
+    ----
+    The relative speed data is fit to the curve generated from the derived
+    speed from the acceleration signal. The gain of this fit is adjusted such
+    that the maximum value of the resulting speed array equals the animal's
+    theoretical maximum speed.This assumes that the animal reaches its maximum
+    theoretical speed at least once during the dataset.
+    '''
+    # TODO problem: observed max could be outlier. outliers in signal/obs could
+    # be filtered
+
+    import numpy
+    import scipy.integrate
+
+    import utils
+
+    x_speed = numpy.zeros(len(rel_speed), dtype=float)
+
+    # Get indices of data sections split by known zero velocities
+    zero_mask = rel_speed > zero_level
+    start_ind, stop_ind = utils.contiguous_regions(zero_mask)
+
+    # Estimate speed in regions between known points of zero velocity
+    for start_idx, stop_idx in zip(start_ind, stop_ind):
+        xi = x[start_idx:stop_idx]
+
+        # Integrate acceleration signal over periods where the relative
+        # velocity sensor data is 'zero' velocity
+        x_int = scipy.integrate.cumtrapz(range(len(xi)), xi, initial=0)
+
+        # Assign total velocity to slice of acc_speed array
+        x_speed[start_idx:stop_idx] = x_int
+
+    # Fit integrated accelerometer speed to relative speed sensor data
+    measured = rel_speed[:]
+    calibration = x_speed/fs_a
+
+    # Generate input array as per numpy docs and apply least-squares regression
+    A = numpy.vstack([measured, numpy.ones(len(measured))]).T
+    gain, offset = numpy.linalg.lstsq(A, calibration)[0]
+
+    # Adjust gain for theoretical max speed of animal, assuming at least one
+    # occurance of max speed
+    gain = (theoretic_max-offset)/measured.max()
+
+    # Apply calibration to relative speed sensor data
+    x_speed_cal = (measured*gain)+offset
+
+    # Set values below zero to NaN if switch set
+    if rm_neg is True:
+        x_speed_cal[x_speed_cal < 0] = numpy.nan
+
+    return x_speed_cal
+
