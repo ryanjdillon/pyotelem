de00bfc98138569a70992a845781136b025e24ab
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-03-16 14:55:16 +0100

    added saving of morpho dataframe to own pickle for table generation

--- utils_experiments.py
+++ utils_experiments.py
@@ -1,5 +1,5 @@
 
-def add_morpho_to_experiments(exp_file, morph_file):
+def add_morpho_to_experiments(exp_file, morpho_file):
     import numpy
     import pandas
 
@@ -13,16 +13,15 @@ def add_morpho_to_experiments(exp_file, morph_file):
     exps = exps[~numpy.isnan(exps['id'])]
 
     # Load tritium analysis and morphometric data, skip first 4 rows
-    morph = pandas.read_csv(morph_file, comment='#')
+    morpho = pandas.read_csv(morpho_file, comment='#')
 
     # Get percent body compositions, including density - what we want
-    perc_comps = utils_morphometrics.lip2dens(morph['fat_perc'])
-    morph['density_kgm3'] = perc_comps['density']*1000
+    perc_comps = utils_morphometrics.lip2dens(morpho['fat_perc'])
+    morpho['density_kgm3'] = perc_comps['density']*1000
 
     # List of columns to add to experiments from tritium-morpho data
-    cols = ['mass_kg', 'water_l', 'water_perc', 'fat_kg', 'fat_perc',
-            'protein_kg', 'protein_perc', 'density_kgm3', 'mass_scale', 'length',
-            'girth']
+    cols = ['mass_kg', 'length', 'girth' 'water_l', 'water_perc', 'fat_kg',
+            'fat_perc', 'protein_kg', 'protein_perc', 'density_kgm3']
 
     # Create new columns in experiment dataframe
     for col in cols:
@@ -32,7 +31,7 @@ def add_morpho_to_experiments(exp_file, morph_file):
     # Add data from tritium-morpho dataframe to experiments dataframe
     for i in range(len(exps)):
         idx = int(exps['tritium_id'].iloc[i])
-        exps.loc[i, cols] = morph.loc[idx, cols]
+        exps.loc[i, cols] = morpho.loc[idx, cols]
 
         # Cacluate total density with modification, buoyant forces
         total_dens = utils_morphometrics.apply_mods(exps['mass_kg'][i],
@@ -43,7 +42,7 @@ def add_morpho_to_experiments(exp_file, morph_file):
         exps.loc[i, 'total_dens'] = total_dens
 
 
-    return exps
+    return exps, morpho
 
 
 if __name__ == '__main__':
@@ -54,11 +53,17 @@ if __name__ == '__main__':
     paths = yaml_tools.read_yaml('./cfg_paths.yaml')
 
     root_path = paths['root']
-    morph_path = paths['bodycondition']
+    morpho_path = paths['bodycondition']
 
-    exp_file = os.path.join(root_path, morph_path, 'coexist_experiments.csv')
-    morph_file = os.path.join(root_path, morph_path, 'coexist_tritium-morphometrics.csv')
+    fname_exp_csv    = 'coexist_experiments.csv'
+    fname_exp_p      = 'coexist_experiments.p'
+    fname_morpho_csv = 'coexist_tritium-morphometrics.csv'
+    fname_morpho_p   = 'coexist_morphometrics.p'
 
-    exps = add_morpho_to_experiments(exp_file, morph_file)
+    exp_file = os.path.join(root_path, morpho_path, fname_exp_csv)
+    morpho_file = os.path.join(root_path, morpho_path, fname_morpho_csv)
 
-    exps.to_pickle(os.path.join(root_path, morph_path, 'coexist_experiments.p'))
+    exps, morpho = add_morpho_to_experiments(exp_file, morpho_file)
+
+    exps.to_pickle(os.path.join(root_path, morpho_path, fname_exp_p))
+    morpho.to_pickle(os.path.join(root_path, morpho_path, fname_morpho_p))
