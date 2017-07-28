046f6db2c59d065e8a039cb658f0215b65452192
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-07-28 14:28:45 +0200

    moved seaborn setup from __init__ to plotconfig.py

--- pyotelem/plots/__init__.py
+++ pyotelem/plots/__init__.py
@@ -1,17 +1,3 @@
-import matplotlib.pyplot as plt
-import seaborn
-import string
-
-# Use specified style (e.g. 'ggplot')
-plt.style.use('seaborn-whitegrid')
-
-# Use specified color palette
-colors = seaborn.color_palette()
-abc = string.ascii_uppercase
-
-# Global axis properties
-linewidth = 0.5
-
 from . import plotdives
 from . import plotdsp
 from . import plotdynamics
