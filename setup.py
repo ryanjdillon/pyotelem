c13abab16479270ce061ba6bca0ad7c91d4e187a
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-12-10 19:11:39 +0100

    Update setup.py with src path

--- setup.py
+++ setup.py
@@ -1,28 +1,34 @@
 from setuptools import setup, find_packages
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+def requirements(fp: str):
+    with open(fp) as f:
+        return [r.strip() for r in f.readlines()]
+
+fp_readme = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+with open(fp_readme, encoding='utf-8') as f:
     long_description = f.read()
+
+setup_requirements = ["pytest-runner", "setuptools_scm"]
 
 setup(
     name='pyotelem',
-    version='0.4',
     description=('Utilities for working with biotelemetry and datalogger '
                  'data in Python'),
     long_description=long_description,
     author='Ryan J. Dillon',
     author_email='ryanjamesdillon@gmail.com',
     url='https://github.com/ryanjdillon/pyotelem',
-    download_url='https://github.com/ryanjdillon/pyotelem/archive/0.4.tar.gz',
     license='GPL-3.0+',
-    packages=find_packages(),
-    install_requires=[
-        'gsw',
-        'pandas',
-        'scipy',
-        'matplotlib'],
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    install_requires=requirements("requirements.in"),
+    tests_requires=requirements("requirements_test.txt"),
     include_package_data=True,
+    use_scm_version={
+        "write_to": "src/pylleo/_version.py",
+        "relative_to": __file__,
+    },
     keywords=['datalogger','accelerometer','biotelemetry'],
     classifiers=[
         'Development Status :: 3 - Alpha',
