98fc1886b258244c6497a6771d877304237d75b6
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-12-10 19:12:53 +0100

    Add version to init

--- src/pyotelem/__init__.py
+++ src/pyotelem/__init__.py
@@ -6,3 +6,8 @@ from . import physio_seal
 from . import plots
 from . import seawater
 from . import utils
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
