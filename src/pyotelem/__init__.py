3c92a368c53631046ed74d073b0af498226567ad
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-12-17 15:59:23 +0100

    Add module/package imports in __init__s

--- src/pyotelem/__init__.py
+++ src/pyotelem/__init__.py
@@ -1,3 +1,11 @@
+from . import dives
+from . import dsp
+from . import dtag
+from . import glides
+from . import seawater
+from . import utils
+from . import plots
+
 try:
     from ._version import version as __version__
 except ImportError:
---
+++ src/pyotelem/plots/__init__.py
@@ -0,0 +1,6 @@
+from . import plotconfig
+from . import plotglides
+from . import plotdives
+from . import plotdynamics
+from . import plotdsp
+from . import plotutils
